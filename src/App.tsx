1c9ce05008f6662c261585bf152fde90af5de963
ashishguleria04 <guleriaashish65@gmail.com>
2025-08-14 20:05:03 +0530

    fixed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { Navbar } from "./components/Navbar";
 import Blog from "./pages/Blog";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 const queryClient = new QueryClient();
